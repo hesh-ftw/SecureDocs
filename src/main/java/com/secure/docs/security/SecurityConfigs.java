40fe758aacef4ecceafeca1f36d4b24e2ce93181
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-08 18:30:11 +0530

    UPDATE : disable csrf token

--- src/main/java/com/secure/docs/security/SecurityConfigs.java
+++ src/main/java/com/secure/docs/security/SecurityConfigs.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,8 +19,11 @@ public class SecurityConfigs {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/public").permitAll()
                 .anyRequest().authenticated());
+
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 }
