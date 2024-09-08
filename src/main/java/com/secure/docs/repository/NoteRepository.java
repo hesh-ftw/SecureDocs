078de23b940904f13ad2ef0104baf724fb8f3359
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-08 16:11:30 +0530

    ADD : NoteRepository

---
+++ src/main/java/com/secure/docs/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.secure.docs.repository;
+import com.secure.docs.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface NoteRepository extends JpaRepository<Note,Long> {
+
+    List<Note> findByUserOwnerUserName(String ownerUsername);
+}
