80d581ede3646b25bec8ceb9557de83f9275617c
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-14 23:33:03 +0530

    ADD : custom user model

---
+++ src/main/java/com/secure/docs/model/User.java
@@ -0,0 +1,84 @@
+package com.secure.docs.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "users",
+            uniqueConstraints={
+                    @UniqueConstraint(columnNames = "username"),
+                    @UniqueConstraint(columnNames = "password")
+            })
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @NotBlank
+    @Column(name = "email")
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "password")
+    @JsonIgnore
+    private String password;
+
+    private boolean accountNonLocked = true;
+    private boolean accountNonExpired = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+
+    private LocalDate credentialsExpiryDate;
+    private LocalDate accountExpiryDate;
+
+    private String twoFactorSecret;
+    private boolean isTwoFactorEnabled = false;
+    private String signUpMethod;
+
+    @ManyToOne()
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @JsonBackReference
+    @ToString.Exclude
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return userId != null && userId.equals(((User) o).getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+
+}
