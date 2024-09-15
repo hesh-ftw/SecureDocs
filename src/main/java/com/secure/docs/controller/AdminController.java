92f4164856c2428ffe2612fff80d7c5e95da3ced
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-15 23:33:06 +0530

    ADD : AdminController which handles users

---
+++ src/main/java/com/secure/docs/controller/AdminController.java
@@ -0,0 +1,48 @@
+package com.secure.docs.controller;
+
+import com.secure.docs.dto.UserDTO;
+
+import com.secure.docs.model.User;
+
+import com.secure.docs.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@AllArgsConstructor
+public class AdminController {
+    private final UserService userService;
+
+    //get all users
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
+    }
+
+    //change user role
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, @RequestParam String roleName){
+        userService.updateUserRole(userId,roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+//    @GetMapping("/user/{id}")
+//    public User getaUserById(@PathVariable Long id){
+//       return userService.getaUserById(id);
+//    }
+
+    //find user by id
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id){
+        return new ResponseEntity<>(userService.getUserById(id),HttpStatus.OK);
+    }
+
+
+}
