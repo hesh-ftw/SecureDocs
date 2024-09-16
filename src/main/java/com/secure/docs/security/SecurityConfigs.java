e5367960caff91997016bdca87aacf324876c25e
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-16 17:39:23 +0530

    ADD : custom users to db for testing

--- src/main/java/com/secure/docs/security/SecurityConfigs.java
+++ src/main/java/com/secure/docs/security/SecurityConfigs.java
@@ -1,24 +1,34 @@
 package com.secure.docs.security;
 
+import com.secure.docs.model.AppRole;
+import com.secure.docs.model.Role;
+import com.secure.docs.model.User;
+import com.secure.docs.repository.RoleRepository;
+import com.secure.docs.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 import javax.sql.DataSource;
-
+import java.time.LocalDate;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfigs {
-
     //defines all the request need to be authenticated  (class SpringBootWebSecurityConfiguration)
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,7 +44,7 @@ public class SecurityConfigs {
 
     //configs of in memory multiple users
 
-//    @Bean
+    //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 //        if (!manager.userExists("user1")) {
@@ -68,4 +78,62 @@ public class SecurityConfigs {
 //        }
 //    return manager;
 //    }
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+            Role newRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+
+            if (!userRepository.existsByUserName("user1")) {
+                User user1 = new User("user1", "user1@example.com", "{noop}password1");
+                user1.setAccountNonLocked(true);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user1.setTwoFactorEnabled(false);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUserName("admin")) {
+                User admin1 = new User("admin", "admin@example.com", "{noop}password");
+                admin1.setAccountNonLocked(true);
+                admin1.setAccountNonExpired(true);
+                admin1.setCredentialsNonExpired(true);
+                admin1.setEnabled(true);
+                admin1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                admin1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                admin1.setTwoFactorEnabled(false);
+                admin1.setSignUpMethod("email");
+                admin1.setRole(adminRole);
+                userRepository.save(admin1);
+            }
+
+            if (!userRepository.existsByUserName("chamath")) {
+                User user2 = new User("chamath", "usercham@example.com", "{noop}champw");
+                user2.setAccountNonLocked(true);
+                user2.setAccountNonExpired(true);
+                user2.setCredentialsNonExpired(true);
+                user2.setEnabled(true);
+                user2.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user2.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user2.setTwoFactorEnabled(false);
+                user2.setSignUpMethod("email");
+                user2.setRole(newRole);
+                userRepository.save(user2);
+            }
+        };
+    }
+
 }
