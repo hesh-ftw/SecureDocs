48b9577b55bce989824c3f7d2724577e3303f4f5
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-26 00:32:59 +0530

    minor change

--- src/main/java/com/secure/docs/repository/UserRepository.java
+++ src/main/java/com/secure/docs/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.secure.docs.repository;
 
 import com.secure.docs.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User>findByUserName(String username);
 
     Boolean existsByUserName(String username);
+    Boolean existsByEmail(String email);
 }
