2c87a7ecb5d1c94012496cfad9fc969d5a991343
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-26 00:31:24 +0530

    minor change

--- src/main/java/com/secure/docs/repository/RoleRepository.java
+++ src/main/java/com/secure/docs/repository/RoleRepository.java
@@ -3,9 +3,10 @@ package com.secure.docs.repository;
 import com.secure.docs.model.AppRole;
 import com.secure.docs.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
     Optional<Role> findByRoleName(AppRole appRole);
 }
