e45d0e83cf9ccb3c73b39051e9c907dbd7c4d603
Chamath Heshan <chamathheshan076@gmail.com>
2024-09-27 23:14:02 +0530

    sign up request dto

---
+++ src/main/java/com/secure/docs/security/request/SignupRequest.java
@@ -0,0 +1,32 @@
+package com.secure.docs.security.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Data
+//dto
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 30)
+    private String password;
+
+    @Getter
+    @Setter
+    private Set<String> role;
+}
+
