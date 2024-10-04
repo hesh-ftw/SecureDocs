82ff28dac1121f07da70828287660eb9e09fd47b
Chamath Heshan <chamathheshan076@gmail.com>
2024-10-04 21:53:45 +0530

    update note service to create logs after operations

--- src/main/java/com/secure/docs/service/NoteService.java
+++ src/main/java/com/secure/docs/service/NoteService.java
@@ -3,6 +3,7 @@ package com.secure.docs.service;
 import com.secure.docs.model.Note;
 import com.secure.docs.repository.NoteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,11 +13,15 @@ import java.util.List;
 public class NoteService {
     private final NoteRepository noteRepository;
 
+    @Autowired
+    AuditLogService auditLogService;
+
     public Note createNoteForUsers(String content, String username){
             Note note = new Note();
             note.setContent(content);
             note.setOwnerUsername(username);
             Note savedNote = noteRepository.save(note);
+            auditLogService.logNoteCreation(username,note);
             return savedNote;
     }
 
@@ -24,11 +29,13 @@ public class NoteService {
             Note note= noteRepository.findById(noteId).orElseThrow(()-> new RuntimeException("Note Not Found"));
             note.setContent(content);
             Note updatedNote = noteRepository.save(note);
+            auditLogService.logNoteUpdate(userName,note);
             return updatedNote;
     }
 
     public void deleteNoteForUsers(Long noteId, String username){
             noteRepository.deleteById(noteId);
+            auditLogService.logNoteDeletion(username,noteId);
     }
 
     public List<Note> getNotesForUser(String username){
