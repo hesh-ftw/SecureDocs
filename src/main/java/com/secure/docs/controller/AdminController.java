29645bf68961b11e21634859bc66f464e43dae7f
Chamath Heshan <chamathheshan076@gmail.com>
2024-10-05 23:54:34 +0530

    add admin actions api endpoints

--- src/main/java/com/secure/docs/controller/AdminController.java
+++ src/main/java/com/secure/docs/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.secure.docs.controller;
 
+import com.secure.docs.model.Role;
 import com.secure.docs.model.User;
 import com.secure.docs.service.UserService;
 import lombok.AllArgsConstructor;
@@ -37,6 +38,46 @@ public class AdminController {
     public ResponseEntity<User> getaUserById(@PathVariable Long id){
         return new ResponseEntity<>(userService.getaUserById(id),HttpStatus.OK);
     }
+
+    @PutMapping("/update-lock-status")
+    public ResponseEntity<String> updateAccountLockStatus(@RequestParam Long userId, @RequestParam boolean lock) {
+        userService.updateAccountLockStatus(userId, lock);
+        return ResponseEntity.ok("Account lock status updated");
+    }
+
+    @GetMapping("/roles")
+    public List<Role> getAllRoles() {
+        return userService.getAllRoles();
+    }
+
+    @PutMapping("/update-expiry-status")
+    public ResponseEntity<String> updateAccountExpiryStatus(@RequestParam Long userId, @RequestParam boolean expire) {
+        userService.updateAccountExpiryStatus(userId, expire);
+        return ResponseEntity.ok("Account expiry status updated");
+    }
+
+    @PutMapping("/update-enabled-status")
+    public ResponseEntity<String> updateAccountEnabledStatus(@RequestParam Long userId, @RequestParam boolean enabled) {
+        userService.updateAccountEnabledStatus(userId, enabled);
+        return ResponseEntity.ok("Account enabled status updated");
+    }
+
+    @PutMapping("/update-credentials-expiry-status")
+    public ResponseEntity<String> updateCredentialsExpiryStatus(@RequestParam Long userId, @RequestParam boolean expire) {
+        userService.updateCredentialsExpiryStatus(userId, expire);
+        return ResponseEntity.ok("Credentials expiry status updated");
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<String> updatePassword(@RequestParam Long userId, @RequestParam String password) {
+        try {
+            userService.updatePassword(userId, password);
+            return ResponseEntity.ok("Password updated");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
 
 
 }
